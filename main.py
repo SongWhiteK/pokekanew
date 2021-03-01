b9da511e9b58d37d5c21d578ca83d9270b08c176
SongWhiteK <73625240+SongWhiteK@users.noreply.github.com>
2021-03-02 00:43:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if "返信" in event.message.text
+    if "返信" in event.message.text:
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=event.message.text))
