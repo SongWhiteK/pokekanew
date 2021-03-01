900b9a54d4ee444c473a57cb9db2e6e769eccc4d
SongWhiteK <73625240+SongWhiteK@users.noreply.github.com>
2021-03-02 02:39:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
+amazon_url = [
+    "https://www.pokemoncenter-online.com/?p_cd=4521329313405",
+    "https://www.pokemoncenter-online.com/?p_cd=4521329266541"
+        ]
+card = ["シャイニースターV" , "TAG TEAM GX BOX"]
+
 @app.route("/")
 def hello_world():
     return "hello world!"
@@ -38,10 +44,24 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if "返信" in event.message.text:
+    if "シャイ" in event.message.text:
+        i = 0
+        data = requests.get(amazon_url[i], headers = my_header)
+        data.encoding = data.apparent_encoding
+        data = data.text
+        soup = BeautifulSoup(data, "html.parser")
+        detail = soup.find("table",class_="no_size")
+        detailstr = str(detail)
+        if "soldout" in detailstr:
+            result_str = "売り切れ"
+        elif "yoyaku" in detailstr:
+            result_str = "予約受付中"
+        elif "add_cart_btn":
+            result_str = "在庫あり"
+
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=event.message.text))
+            TextSendMessage(text=amazon_url[i]+result_str))
 
 if __name__ == "__main__":
 #    app.run()
